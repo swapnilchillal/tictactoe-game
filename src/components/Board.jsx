3f650f36b80477a4ade61e4444f97e1c0959c2b1
Swapnil Chillalnode <srchillal589@gmail.com>
2023-05-22 22:10:22 +0530

    added player X, O

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,54 @@
+import { useState } from 'react';
 import Square from './Square';
+
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleSquareClick = positionClicked => {
+
+    if(squares[positionClicked]){
+      return;
+    }
+    setSquares(currentSquare => {
+      return currentSquare.map((squareValue, pos) => {
+        if (positionClicked === pos) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return squareValue;
+      });
+    });
+
+    setIsXNext((currentIsXNext) => !currentIsXNext);
+
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square
+        value={squares[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
